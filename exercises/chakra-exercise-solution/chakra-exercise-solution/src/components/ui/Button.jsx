1057382a9c71957ea26885a024ae7dd2f64d9cd9
Arthur Baars <arthurbaars@outlook.com>
2025-08-21 11:30:19 +0200

    Cleanup and hierarchical restructuring

--- exercises/chakra-exercise-solution/chakra-exercise-solution/src/components/ui/Button.jsx
+++
@@ -1,7 +0,0 @@
-import { Button as CButton } from '@chakra-ui/react';
-
-export const Button = ({ clickFn, ...props }) => (
-  <CButton colorScheme="teal" onClick={clickFn} {...props}>
-    {props.children}
-  </CButton>
-);
--- exercises/chakra-exercise-solution/chakra-exercise-solution/src/components/ui/TextInput.jsx
+++
@@ -1,5 +0,0 @@
-import { Input } from '@chakra-ui/react';
-
-export const TextInput = ({ changeFn, ...props }) => (
-  <Input variant="flushed" onChange={changeFn} {...props} />
-);
