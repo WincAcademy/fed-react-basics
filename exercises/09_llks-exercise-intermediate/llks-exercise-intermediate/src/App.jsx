60d58370342fc21597127d05434cc14c628114a4
Arthur Baars <arthurbaars@grachtwerk.nl>
2025-08-22 14:13:55 +0200

    Added 09_llks-exercise-intermediate

---
+++ exercises/09_llks-exercise-intermediate/llks-exercise-intermediate/src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+import { WeatherIcon } from './components/WeatherIcon'
+import {
+    sunnyWeather,
+    cloudyWeather,
+    rainyWeather
+} from './utils/data'
+import { useState } from 'react'
+
+export const App = () => {
+    const [weather, setWeather] = useState(sunnyWeather);
+    const weatherTypes = [
+        sunnyWeather,
+        cloudyWeather,
+        rainyWeather
+    ];
+
+    return (
+        <>
+            <h1>Winc Weather App</h1>
+            { weather && <WeatherIcon weather={weather} /> }
+            
+            <div className='button-container'>
+            
+            </div>
+        </>
+    )
+}
