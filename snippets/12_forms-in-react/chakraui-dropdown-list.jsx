05b892767f04105e0d5f699586de942f6e9993d6
Arthur Baars <arthurbaars@outlook.com>
2025-09-04 11:37:24 +0200

    Added more code snippets for 12_forms-in-react

---
+++ snippets/12_forms-in-react/chakraui-dropdown-list.jsx
@@ -0,0 +1,12 @@
+<Field.Root>
+    <Field.Label>Favorite fruit</Field.Label>
+    <NativeSelect defaultValue="">
+        <option value="" disabled>
+            Select a fruit
+        </option>
+        <option value="apple">Apple</option>
+        <option value="banana">Banana</option>
+        <option value="orange">Orange</option>
+    </NativeSelect>
+    <Field.HelperText>You can only pick one.</Field.HelperText>
+</Field.Root>
---
+++ snippets/12_forms-in-react/chakraui-radio-buttons.jsx
@@ -0,0 +1,9 @@
+<Field.Root>
+    <Field.Label>Preferred contact method</Field.Label>
+    <RadioGroup defaultValue="email">
+        <Radio value="email">Email</Radio>
+        <Radio value="phone">Phone</Radio>
+        <Radio value="mail">Mail</Radio>
+    </RadioGroup>
+    <Field.HelperText>Choose one!</Field.HelperText>
+</Field.Root>
