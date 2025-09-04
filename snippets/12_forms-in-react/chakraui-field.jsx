e42a5a66d7c1e34e016f549f23aa3bac815faa30
Arthur Baars <arthurbaars@outlook.com>
2025-09-04 11:26:45 +0200

    Updated one code snippet

--- snippets/12_forms-in-react/chakraui-field.jsx
+++ snippets/12_forms-in-react/chakraui-field.jsx
@@ -1,7 +1,14 @@
+const [email, setEmail] = React.useState('');
+
 <Field.Root invalid={errors.email}>
     <Field.Label>Email</Field.Label>
 
-    <Input placeholder="you@example.com" />
+    <Input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="johndoe@email.com"
+    />
 
     <Field.HelperText>We'll never share your email!</Field.HelperText>
     { errors.email && <Field.ErrorText>{errors.email}</Field.ErrorText> }
