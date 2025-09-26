3680a5ddcd1743d73c53f58d4d23b1c9c735dfef
Arthur Baars <arthurbaars@grachtwerk.nl>
2025-09-26 16:05:21 +0200

    Updated vite-template eslint.config.js as it visually differs

--- templates/vite-template/vite-template/eslint.config.js
+++ templates/vite-template/vite-template/eslint.config.js
@@ -4,8 +4,8 @@ import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended, pluginReact.configs.flat["jsx-runtime"],
-  { rules: { "react/prop-types": "off",  } }
+  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat["jsx-runtime"],
+  { rules: { "react/prop-types": "off" } }
 ]);
