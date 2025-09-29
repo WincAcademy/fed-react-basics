a28b93ce9d777e4a1662d274923625c121d8f658
Arthur Baars <arthurbaars@outlook.com>
2025-09-29 12:18:38 +0200

    Modified code snippet

--- snippets/09_llks/weather-types-map-02.jsx
+++
@@ -1,9 +0,0 @@
-<div className='button-container'>
-    {weatherObjects.map((weatherObject) => (
-        <button
-            onClick={() => setWeather(weatherObject)}
-        >
-            {weatherObject.weatherType}
-        </button>
-    ))}
-</div>
