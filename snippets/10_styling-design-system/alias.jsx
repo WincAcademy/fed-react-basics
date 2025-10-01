43a553edc880862707bfaff3c48e6cc8cb889c6c
Arthur Baars <arthurbaars@grachtwerk.nl>
2025-10-01 15:12:27 +0200

    Updated code snippet

--- snippets/10_styling-design-system/alias.jsx
+++ snippets/10_styling-design-system/alias.jsx
@@ -1,1 +1,10 @@
 import { Container as MyContainer } from './components/Container';
+
+export const App = () => {
+    // We now need to use MyContainer instead of Container!
+    return (
+        <MyContainer>
+            <p>Hello world!</p>
+        </MyContainer>
+    )
+};
